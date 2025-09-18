f5d720a25f18e144b144be42ca116cc95f684b59
EvaArbo <evelynearbogusti@gmail.com>
2025-09-19 01:22:08 +0300

    modified skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,23 +39,23 @@ export const  Skills =()=>{
                            learning new tools to improve my craft.</p>
                         <Carousel responsive={responsive} infinite={true} className="skill-slider">
                             <div className="items">
-                                <img src={meter1} alt="Progress meter showing web development skills"/>
-                                <h5>Web Development</h5>
+                                <img src={meter1} alt="Progress meter showing front& back-end development skills"/>
+                                <h5>Front&Back-End Development</h5>
 
                             </div>
                             <div className="items">
-                                <img src={meter2} alt="Progress meter representing brand identity expertise"/>
-                                <h5>Brand Identity</h5>
+                                <img src={meter2} alt="Progress meter highlighting creative UI and UX design"/>
+                                <h5>Creative UI/UX Design</h5>
 
                             </div>
                             <div className="items">
-                                <img src={meter3} alt="Progress meter illustrating logo design ability"/>
-                                <h5>Logo Design</h5>
+                                <img src={meter3} alt="Progress meter showing responsive design proficiency"/>
+                                <h5>Responsive Layouts</h5>
 
                             </div>
                             <div className="items">
-                                <img src={meter1} alt="Progress meter for front-end development proficiency"/>
-                                <h5>Web Development</h5>
+                                <img src={meter1} alt="Progress meter showing ability to build interactive applications"/>
+                                <h5>Interactive Web Apps</h5>
 
                             </div>
                         </Carousel>
